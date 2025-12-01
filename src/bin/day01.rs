96905102cba45457ef3ccdc617c9db27f5c37b3a
Nathan Andrews <nandrews.programming@gmail.com>
2025-12-01 00:22:25 -0500

    day 01 part 1

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,5 +1,32 @@
-use advent_of_code_2025::read_input_file;
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let file = File::open(
+        "problem-inputs/day01-1.txt",
+    )?;
+    let reader = BufReader::new(file);
+
+    let mut answer = 0;
+    let mut position = 50;
+    for line_result in reader.lines() {
+        let line = line_result?;
+       if let Some(direction) = line.chars().nth(0) && let Ok(mut magnitude) = line[1..].parse::<i32>()  {
+            if direction == 'L' {
+                magnitude = magnitude * -1;
+            }
+
+            position = position + magnitude;
+            position = position % 100;
+            if position == 0 {
+                answer += 1
+            }
+        }
+    }
+
+    println!("[part 1] num times at zero: {}", answer);
+
+    return Ok(());
 }
